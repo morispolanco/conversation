4ee3b34cbbfc755a391d448917678b1b65d233d9
morispolanco <morispolanco@gmail.com>
2023-07-27 03:12:30 -0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,17 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = '8af2c01e-bf0a-4429-984c-33d7f5ccf029'
+export const API_KEY = 'app-xDUc8fUHpObAhSiSRO6N1QPE'
+export const API_URL = 'https://udify.app/chatbot/6QkO7PsJLKHGKCfI'
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'LeybotGt',
+  description: 'Haz preguntas sobre las leyes de Guatemala',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'es-ES',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'I want you to act as a legal advisor.'
 
 export const API_PREFIX = '/api'
 
